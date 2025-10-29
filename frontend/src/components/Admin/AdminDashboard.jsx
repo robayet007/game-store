5bf4d8f313223ac527aee11aa29f4f55eec414b1
robayet007 <mdrobayet007@gmail.com>
2025-10-29 09:44:45 +0600

    duck u

--- frontend/src/components/Admin/AdminDashboard.jsx
+++ frontend/src/components/Admin/AdminDashboard.jsx
@@ -3,7 +3,7 @@ import React, { useState, useEffect } from 'react';
 import { useNavigate, Routes, Route, useLocation } from 'react-router-dom';
 import { signOut } from 'firebase/auth';
 import { auth } from '../../firebaseConfig';
-import { isAdminUser } from '../../Utils/Admin';
+import { isAdminUser } from '../../Utils/admin';
 import Sidebar from './Sidebar/Sidebar';
 import Dashboard from './Dashboard/Dashboard'; // ✅ Import Dashboard
 import AddProduct from './AddProduct/AddProduct';
